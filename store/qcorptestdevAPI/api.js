bedf24c09f4ec6b1474ede014f5a3bd7edb8e761
Crowdbotics <engineering@crowdbotics.com>
2023-05-21 23:12:47 +0000

    Update store.

--- store/qcorptestdevAPI/api.js
+++ store/qcorptestdevAPI/api.js
@@ -3,6 +3,12 @@ const qcorptestdevAPI = axios.create({
   baseURL: "https://qcorp-test-dev-85146.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_home_list(payload) {
+  return qcorptestdevAPI.get(`/api/v1/home/`)
+}
+function api_v1_home_create(payload) {
+  return qcorptestdevAPI.post(`/api/v1/home/`, payload.data)
+}
 function api_v1_login_create(payload) {
   return qcorptestdevAPI.post(`/api/v1/login/`, payload.data)
 }
@@ -25,6 +31,18 @@ function api_docs_schema_retrieve(payload) {
 }
 function rest_auth_login_create(payload) {
   return qcorptestdevAPI.post(`/rest-auth/login/`, payload.data)
+}
+function api_v1_home_retrieve(payload) {
+  return qcorptestdevAPI.get(`/api/v1/home/${payload.id}/`)
+}
+function api_v1_home_update(payload) {
+  return qcorptestdevAPI.put(`/api/v1/home/${payload.id}/`, payload.data)
+}
+function api_v1_home_partial_update(payload) {
+  return qcorptestdevAPI.patch(`/api/v1/home/${payload.id}/`, payload.data)
+}
+function api_v1_home_destroy(payload) {
+  return qcorptestdevAPI.delete(`/api/v1/home/${payload.id}/`)
 }
 function rest_auth_logout_retrieve(payload) {
   return qcorptestdevAPI.get(`/rest-auth/logout/`)
@@ -75,6 +93,8 @@ function rest_auth_registration_verify_email_create(payload) {
   )
 }
 export const apiService = {
+  api_v1_home_list,
+  api_v1_home_create,
   api_v1_login_create,
   api_v1_signup_create,
   rest_auth_user_retrieve,
@@ -82,6 +102,10 @@ export const apiService = {
   rest_auth_user_partial_update,
   api_docs_schema_retrieve,
   rest_auth_login_create,
+  api_v1_home_retrieve,
+  api_v1_home_update,
+  api_v1_home_partial_update,
+  api_v1_home_destroy,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
   api_v1_employment_list,
