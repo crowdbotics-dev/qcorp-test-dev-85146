d220843c7dd80e570468864356c901b792b4a139
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-21 23:09:10 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+class Home(models.Model):
+    'Generated Model'
+    address = models.CharField(max_length=100,)
+    city = models.CharField(max_length=100,)
+    state = models.CharField(max_length=2,)
+    zip = models.CharField(max_length=10,)
+    created_at = models.DateTimeField(auto_now_add=True,)
+    updated_at = models.DateTimeField(auto_now=True,)
