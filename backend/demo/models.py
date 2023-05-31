e1e26504d22cc323ca22ceddcc88d0071e1ce9b9
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-31 18:55:53 +0000

    Update models

--- backend/demo/models.py
+++ backend/demo/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class DemoFields(models.Model):
     'Generated Model'
     quan_is_the_best = models.TextField()
+    witch_hunt = models.BigIntegerField(null=True,blank=True,)
 
 # Create your models here.
